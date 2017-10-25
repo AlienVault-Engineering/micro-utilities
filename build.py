2a7856498279d22c9c11f73f9124c7da87fb5ac5
rspitler <rspitler@alienvault.com>
2017-10-25 15:53:52 -0500

    set build number

--- build.py
+++ build.py
@@ -7,15 +7,18 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
-
 name = "micro-utilities"
 default_task = "publish"
 
 
 @init
 def set_properties(project):
+    build_number = project.get_property("build_number")
+    if build_number is not None and "" != build_number:
+        project.version = build_number
+    else:
+        project.version = "0.0.999"
     project.depends_on_requirements("src/main/python/requirements.txt")
     project.set_property('unittest_module_glob', 'test_*')
     project.set_property('coverage_branch_threshold_warn', 40)
     project.set_property('coverage_branch_partial_threshold_warn', 40)
-
